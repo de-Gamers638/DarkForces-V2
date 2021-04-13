b1b0f00092de714411eb82dd64c8042a7ff985e2
de-Gamers638 <63507029+de-Gamers638@users.noreply.github.com>
2021-04-13 11:54:09 +0200

    Basic timer implemantation

--- Client/Utils/Timer.cs
+++ Client/Utils/Timer.cs
@@ -1,1 +1,35 @@
-
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; 
+namespace Client.Utils
+{ 
+     public class Timer 
+     {
+          private int currentTick = 0;     
+          
+          public static void Reset()
+          {
+              currentTick = 0;
+          }
+          
+          public static void timerHasReached(int value, bool reset)
+          {
+              if (currentTick == value)
+              {
+                  
+                  if (reset)
+                  {
+                     reset();
+                  }
+                  return true;
+              }
+              else
+              {
+                  return false;
+              
+              }     
+          }
+    }   
+}
